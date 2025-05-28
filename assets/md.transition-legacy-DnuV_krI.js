94b84146a81e410b64d0ef56c27bec9ebc22bd12
NHEL360 <nhelmeynard@gmail.com>
2025-05-28 15:00:30 +0800

    Updates

--- assets/md.transition-legacy-DnuV_krI.js
+++ assets/md.transition-legacy-DnuV_krI.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C8Hsb5zm.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BOChZouA.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
