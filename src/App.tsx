9831d9ba0365f1ffd105adf9ec8411403d2011de
NHEL360 <nhelmeynard@gmail.com>
2025-05-28 15:19:04 +0800

    fixing bugs

--- src/App.tsx
+++ src/App.tsx
@@ -18,15 +18,6 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
 import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
@@ -45,7 +36,7 @@ const App: React.FC = () => (
         <Route exact path="/appdev-attendancetracker" component={Login} />
         <Route exact path="/appdev-attendancetracker/register" component={Register} />
         <Route path="/appdev-attendancetracker/app" component={Menu} />
-        <Redirect exact from="/" to="/appdev-attendancetracker" />
+
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
